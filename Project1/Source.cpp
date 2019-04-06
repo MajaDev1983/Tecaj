3f726a865cb5cd10704b89839fde25fba56f3807
RenatoZaneto <majastipetic22@gmail.com>
2019-04-06 15:31:59 +0200

    Igra

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,33 +1,101 @@
 #include <iostream>
 #include <string>
 
+class Player
+{
+
+public:
+	Player(int positionX, int positionY, int currentVelocity, int currentHp)
+	{
+		x = positionX;
+		y = positionY;
+		velocity = currentVelocity;
+		hp = currentHp;
+	}
+
+	std::string userName;
+	int x, y, velocity, hp, strenght;
+
+	void move()
+	{
+		x += velocity;
+		y += velocity;
+	}
+
+	void damage(int ammount)
+	{
+		hp -= ammount;
+	}
+};
+
+
+
+void move(int& x, int& y, int& velocity)
+{
+	x += velocity;
+	y += velocity;
+}
+
 int main()
 {
-	int number = 5;
-	int& tracker = number;
-	int* ptr = &number;
-	std::string* stringptr;
-	//void* ptr = &number;
-	std::string someWord = "Nesto";
-	//ptr = (void*)&someWord;
-	stringptr = &someWord;
-	int** doubleptr = &ptr;
-
-	number = 9;
+	Player playerOne = Player(0, 0, 10, 30);
+	playerOne.userName = "Chaos";
 	
-	std::cout << "Hello world" << std::endl;
-	std::cout << "number: " << number << std::endl;
-	std::cout << "tracker: " << tracker << std::endl;
-	std::cout << "ptr: " << ptr << std::endl;
-	std::cout << "stringptr: " << stringptr << std::endl;
+	//move(playerOneX, playerOneY, velocity); // pass by value, ali se stavi gore u funkciju & i to je by reference
 
-	std::cout << "derefference strinptr: " << *stringptr << std::endl;
-	*stringptr = "nesto drugo";
-	std::cout << "derefference strinptr take two: " << *stringptr << std::endl;
-	std::cout << "doubleptr ptr address: " << *doubleptr << std::endl;
-	std::cout << "value: " << **doubleptr << std::endl;
+	Player playerTwo = Player(10, 15, 10, 30);
+	playerTwo.userName = "robi";
+
+
+	playerOne.move();
+	playerTwo.move();
+
+	std::string losingPlayer;
+	std::string winnigPlayer;
+
+	while (playerOne.hp > 0 && playerTwo.hp > 0)
+	{
+		int check;
+		std::cout << "Choose whitch player you want to hit: ";
+		std::cin >> check;
+		std::cout << std::endl;
+
+		std::cin.ignore(1000, '\n');
+		std::cin.clear();
+		switch (check)
+		{
+		case 1:
+			playerOne.damage(5);
+			std::cout << "Player one lost 5 hitpoints" << std::endl;
+			break;
+		case 2:
+			playerTwo.damage(5);
+			std::cout << "Player two lost 5 hitpoints" << std::endl;
+			break;
+		default:
+			break;
+		}
+
+		std::cout << "Player one hitpoints: " << playerOne.hp << std::endl;
+		std::cout << "Player two hitpoints: " << playerTwo.hp << std::endl;
+	}
+
+	if(playerOne.hp <= 0)
+	{
+		losingPlayer = playerOne.userName;
+	}
+	else if(playerTwo.hp <= 0)
+	{
+		losingPlayer = playerTwo.userName;
+	}
+
+	std::cout << winningPlayer << "wins! Congratulations!" << std::endl;
+	std::cout << losingPlayer << " lost! Better luck next time!";
 
 	std::cin.get();
+
+	//napravi još jednog playera, te postaviti mogucnost za biranje usernamea.
+	//prosiriti mogucnost dameganja playera tako da se bira koji ce kojeg udariti.
 
 	return 0;
 }
