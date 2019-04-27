218a8c256173389ea15813896feffe6af9e2bfc1
MajaDev1983 <majastipetic22@gmail.com>
2019-04-27 13:32:13 +0200

    Klase nasljeđivanje

---
+++ klaseMaloVise/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Vector3
+{
+	float x, y, z;
+};
+
+class Entity
+{
+public:
+	Vector3 position;
+
+protected:
+	std::string name;
+};
+
+class Player : public Entity
+{
+public:
+
+	Vector3 getPositiong()
+	{
+		return position;
+	}
+
+	std::string getName()
+	{
+		return name;
+	}
+};
+
+class Enemy : public Entity
+{
+public:
+
+};
+
+
+int main()
+{
+	Entity entity = Entity();
+	Vector3 vector = Vector3();
+	Player player = Player();
+	Enemy enemy = Enemy();
+
+	player.position;
+	enemy.position;
+	
+
+	std::cout << player.getName() << std::endl;
+	std::cin.get();
+
+
+}
