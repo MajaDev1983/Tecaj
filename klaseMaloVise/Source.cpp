74e0e68f6e3b5a6c792847515ab320bcfdf983de
MajaDev1983 <majastipetic22@gmail.com>
2019-04-27 15:14:59 +0200

    osobe treba jos dovrsiti

--- klaseMaloVise/Source.cpp
+++ klaseMaloVise/Source.cpp
@@ -2,54 +2,149 @@
 #include <string>
 #include <vector>
 
-struct Vector3
-{
-	float x, y, z;
-};
-
-class Entity
+class Person
 {
 public:
-	Vector3 position;
-
-protected:
-	std::string name;
-};
-
-class Player : public Entity
-{
-public:
-
-	Vector3 getPositiong()
+	
+	Person(std::string givenName, std::string givenLastName, std::string givenAddress)
 	{
-		return position;
+		_name = givenName;
+		_lastName = givenLastName;
+		_address = givenAddress;
 	}
 
-	std::string getName()
+	~Person();
+	
+	bool trysetSSN(std::string ssn)
 	{
-		return name;
+		if (_socialSecuritiNumber != "")
+		{
+			std::cout << "Social Security Number is already set!" << std::endl;
+			std::cout << "Persons SSN cannot be changed!" << std::endl;
+			return true;
+		}
+
+		if (ssn.size() != 11)
+		{
+			std::cout << "Social Security Number is invalid! Wrong numer of characters!" << std::endl;
+			return false;
+		}
+
+		for (size_t i = 0; i < ssn.size(); i++)
+		{
+			char current = ssn[i];
+			if (current < 0 || current > 9)
+			{
+				std::cout << "Social Security Number is invalid!" << std::endl;
+				std::cout << "Character at index " << i << " is invalid!" << std::endl;
+				return false;
+			}
+		}
+		_socialSecuritiNumber = ssn;
+		return true;
+	}
+
+	void print()
+	{
+		std::cout << "Name: " << _name << " " << _lastName << std::endl;
+		std::cout << "Address: " << _address << " " << std::endl;
+		std::cout << "Social Security Number: " << _socialSecuritiNumber << std::endl;
+	}
+
+private:
+	std::string _name, _lastName, _address, _socialSecuritiNumber;
+};
+
+void dispalyPersons(std::vector<Person> persons)
+{
+	for (size_t i = 0; i < persons.size(); i++)
+	{
+		persons[i].print;
+	}
+}
+
+void inputNewData(std::string message, std::string& data)
+{
+	std::cout << "Enter new persons name: " << std::endl;
+	std::cin >> name;
+	std::cin.clear();
+	std::cin.ignore(10000, '\n');
+}
+
+void createPerson(std::vector<Person>& person)
+{
+	std::string name, lastName, address;
+	
+	std::cout << "Enter new persons name: " << std::endl;
+	std::cin >> name;
+	std::cin.clear();
+	std::cin.ignore(10000, '\n');
+
+	std::cout << "Enter new persons laste name: " << std::endl;
+	std::cin >> lastName;
+	std::cin.clear();
+	std::cin.ignore(10000, '\n');
+
+	std::cout << "Enter new persons address: " << std::endl;
+	std::cin >> address;
+	std::cin.clear();
+	std::cin.ignore(10000, '\n');
+
+
+	Person person = Person(name, lastName, address);
+	std::string ssn;
+
+	while (true)
+	{
+		std::cout << "Enter new social security number: " << std::endl;
+		std::cin >> ssn;
+		if (person.trysetSSN(ssn))
+		{
+
+		}
+
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
 	}
 };
 
-class Enemy : public Entity
-{
-public:
-
-};
 
 
 int main()
 {
-	Entity entity = Entity();
-	Vector3 vector = Vector3();
-	Player player = Player();
-	Enemy enemy = Enemy();
+	//Zadatak:
+	//Napraviti klasu osobe, sadržava ime, prezime, adresu, oib
+	//Konstruktor treba inicijalizirate sve memebere osim oiba
+	//Posebna metoda za setanje oiba, probjeriti jel valjan oib ili postojeci, te na temelju toga namjestiti oib
+	//Napraviti probram koji omogucuje izradu novih osoba i iima mogucnost printanja svih unesenih osoba
 
-	player.position;
-	enemy.position;
-	
+	std::vector<Person> persons;
 
-	std::cout << player.getName() << std::endl;
+	while (true)
+	{
+		std::cout << "What do you want to do: " << std::endl;
+		std::cout << "1 - Add new person" << std::endl;
+		std::cout << "2 - Exit" << std::endl;
+		
+		int choice;
+		std::cin >> choice;
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+
+		switch (choice)
+		{
+		case 1:
+			createPerson(persons);
+			break;
+		case 2:
+			return;
+		default:
+			break;
+		}
+
+
+	}
+
 	std::cin.get();
 
 
