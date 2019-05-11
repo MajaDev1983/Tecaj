8d6a51e171b0253b93403ee66ba42689261c8a39
MajaDev1983 <majastipetic22@gmail.com>
2019-05-11 13:52:41 +0200

    malo više o polimorfizmu

--- Project4/Source.cpp
+++ Project4/Source.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//polimorfizam vježba - RADI SAMO NA HEAPU SAMO RADI; ALI NE NA STACKU!!!
-//virtual function table vježba - napravi tablicu i gleda koja je zapravo definicija funckije koja se naslijedila
+//polimorfizam vježba - RADI SAMO NA HEAPU, ALI NE NA STACKU!!! svaki auto je vozilo, ali svako vozilo nije auto. Radi se sa pointerima transformacije...
+//virtual function table vježba - napravi tablicu i gleda koja je zapravo definicija funckije koja se naslijedila.
 
 class Human
 {
@@ -17,6 +17,8 @@ public:
 		std::cout << "I am a human being!" << std::endl;
 	}
 
+	virtual int getNumberOfHands() = 0; //pure virutal function (nema implentaciju u baznoj klasi), mora postojati implementacija unutar klasa koje naslijeðuju tu klasu - služi kao interface
+
 private:
 
 };
@@ -29,6 +31,11 @@ public:
 		std::cout << "I am a male human being!" << std::endl;
 	}
 
+	int getNumberOfHands()
+	{
+		return hands;
+	}
+
 private:
 
 };
@@ -39,6 +46,11 @@ public:
 	void PrintType()
 	{
 		std::cout << "I am a female human being!" << std::endl;
+	}
+
+	int getNumberOfHands()
+	{
+		return hands;
 	}
 private:
 
@@ -56,19 +68,23 @@ int main()
 	Female* femaleObject = new Female();
 	femaleObject->PrintType();
 
-	Human* humanObject = new Human();
+	//Human* humanObject = new Human(); //zakomentirano da bi se pokazala kako radi true virtual
 
 	std::vector<Human*> humans;
 	humans.push_back(maleObject);
 	humans.push_back(femaleObject);
-	humans.push_back(humanObject);
+	//humans.push_back(humanObject);
 
 	std::cout << "==========================================" << std::endl;
 
 	for (size_t i = 0; i < humans.size(); i++)
 	{
 		humans[i]->PrintType();
+		std::cout << humans[i]->getNumberOfHands() << std::endl;
 	}
 
 	std::cin.get();
+
+	delete femaleObject;
+	delete maleObject;
 }
